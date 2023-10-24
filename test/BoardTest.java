d56f231d01ab8247c7b01270f974eb0d5b30de5b
MasterDani10 <143663112+MasterDani10@users.noreply.github.com>
2023-10-23 18:28:35 -0600

    Created the test BoardTest and added various test methods

---
+++ test/BoardTest.java
@@ -0,0 +1,91 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+
+    Board board = new Board();
+    Player luis = new Player("Luis");
+    Player ben = new Player("Ben");
+    int x, y = 0;
+    @BeforeEach
+    void setUp(){
+        board = new Board();
+        board.selectPlayerOne(luis);
+        board.selectPlayerTwo(ben);
+    }
+    @Test
+    void size() {
+        assertEquals(15,board.size);
+    }
+    @Test
+    void size2() {
+        int size = 17;
+        Board board2 = new Board(size);
+        assertEquals(size,board2.size);
+    }
+
+    @Test
+    void clear() {
+
+    }
+
+    @Test
+    void isFull() {
+
+    }
+
+    @Test
+    void placeStone() {
+        board.placeStone(x,y,luis);
+        assertTrue(board.isOccupiedBy(x,y,luis));
+    }
+
+    @Test
+    void isEmpty() {
+        board.placeStone(x,y,luis);
+        assertFalse(board.isEmpty(x,y));
+    }
+
+    @Test
+    void isOccupied() {
+        board.placeStone(x,y,luis);
+        assertTrue(board.isOccupied(x,y));
+    }
+
+    @Test
+    void isOccupiedBy() {
+        board.placeStone(x,y,luis);
+        assertTrue(board.isOccupiedBy(x,y,luis));
+    }
+
+    @Test
+    void playerAt() {
+        board.placeStone(x,y,luis);
+        assertEquals(luis.name(),board.playerAt(x,y).name());
+    }
+
+//    @Test
+//    void isWonBy() {
+//    }
+//
+//    @Test
+//    void winningRow() {
+//    }
+
+    @Test
+    void selectPlayerOne(){
+        Player daniel = new Player("Daniel");
+        board.selectPlayerOne(daniel);
+        board.placeStone(7,7,daniel);
+        assertEquals("Daniel",board.playerAt(7,7).name());
+    }
+    @Test
+    void selectPlayerTwo(){
+        Player alex = new Player("Alex");
+        board.selectPlayerOne(alex);
+        board.placeStone(5,5,alex);
+        assertEquals("Alex",board.playerAt(5,5).name());
+    }
+}
